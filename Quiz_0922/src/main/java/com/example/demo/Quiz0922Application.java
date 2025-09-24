5fac0dacb4f2046270abf68aef0e4632b2c2264a
737genie <kkkh_1007@naver.com>
2025-09-24 14:19:40 +0900

    first commit

---
+++ Quiz_0922/src/main/java/com/example/demo/Quiz0922Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz0922Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz0922Application.class, args);
+	}
+
+}
---
+++ Quiz_0922/src/test/java/com/example/demo/Quiz0922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz0922ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
