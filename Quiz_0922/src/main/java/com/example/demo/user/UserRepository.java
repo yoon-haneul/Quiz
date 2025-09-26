9bb8b1e26388dee657a02e69e9825b70a690e359
yoon-haneul <bm000227@gmail.com>
2025-09-26 13:56:23 +0900

    0926

--- Quiz_0922/src/main/java/com/example/demo/user/UserRepository.java
+++ Quiz_0922/src/main/java/com/example/demo/user/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUsers, Long> {
 
-	//Optional<SiteUsers> findByUsername(String username); //로그인 처리를 위한 회원찾기
+
+	Optional<SiteUsers> findByUsername(String username); //로그인 처리를 위한 회원찾기
 
 }
